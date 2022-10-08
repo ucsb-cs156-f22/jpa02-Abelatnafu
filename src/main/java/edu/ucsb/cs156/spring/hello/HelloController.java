0bb66bb00a7eb18c3d9fb263ac08c3b2c1f96e9f
Abelatnafu <Abeljayirv@gmail.com>
2022-10-08 15:09:53 -0700

    added ! in the index

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from abelatnafu";
+        return "Greetings from abelatnafu!";
     }
     
 }
